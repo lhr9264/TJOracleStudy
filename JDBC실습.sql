3956b325d9459ad36d42c3d07aa8511d52fc2cb8
lhr9264 <lkj112621@gmail.com>
2024-02-22 13:51:35 +0900

    2024년 2월 22일 수업자료

--- JDBC실습.sql
+++ JDBC실습.sql
@@ -216,3 +216,88 @@ print member_auth;
 --아이디가 틀린 경우 : 0
 execute MyMemberAuth('yugyeom아이디틀림','1234',:member_auth);
 print member_auth;
+
+
+
+
+/**********************
+JSP 웹 프로그래밍 - JDBC실습
+***********************/
+
+--새로운 사용자 계정 생성을 위해 system 계정 연결
+
+--계정 생성
+create user musthave identified by 1234;
+
+--권한 부여
+grant connect, resource to musthave;
+
+--새 계정으로 오라클 접속
+/*
+CMD환경에서 sqlplus를 통해 접속한 경우에는 다른 계정으로 전환시 아래와 같이
+conn (혹은 connect) 명령어를 사용할 수 있다. 하지만 SQL디벨로퍼에서는
+사용할 수 없는 대신 우측 상단의 셀렉트박스를 통해 계정을 변경할 수 있다.
+*/
+conn musthave/1234;
+show user;
+
+grant connect, resource, unlimited tablespace to musthave;
+
+grant connect, resource, unlimited tablespace to musthave;
+
+--테이블 목록 조회
+select * from tab;
+
+--테이블 및 시퀀스 생성을 위해 musthave 계정 연결
+--기존에 생성된 테이블이 있따면 삭제 후 다시 만들 수 있다.
+drop table member;
+drop table board;
+drop sequence seq_board_num;
+
+--회원 테이블 생성
+create table member (
+    id varchar2(10) not null,
+    pass varchar2(10) not null,
+    name varchar2(30) not null,/* 회원제 게시판이므로 회원아이디 필요 */
+    regidate date default sysdate not null,/* 게시물의 작성일 */
+    primary key (id)/* 게시물의 조회수 */
+);
+
+--모델1 방식의 게시판 테이블 생성
+create table board (
+    num number primary key,
+    title varchar2(200) not null,
+    content varchar2(2000) not null,
+    id varchar2(10) not null,
+    postdate date default sysdate not null,
+    visitcount number(6)
+);
+
+--외래키 설정
+/*
+자식테이블인 board가 부모테이블인 member를 참조하는 외래키를 설정한다.
+board의 id칼럼이 member의 기본키인 id칼럼을 참조한다.
+*/
+alter table board
+    add constraint board_mem_fk foreign key (id)
+    references member (id);
+    
+--시퀀스 생성
+--board테이블에 중복되지 않는 일련번호 부여를 위해 사용
+create sequence seq_board_num
+    increment by 1
+    start with 1
+    minvalue 1
+    nomaxvalue
+    nocycle
+    nocache;
+    
+--더미 데이터 입력
+insert into member (id,pass, name) values ('musthave', '1234', '머스트해브');
+insert into board (num, title, content, id, postdate, visitcount)
+    values (seq_board_num.nextval, '제목1입니다.', '내용1입니다.', 'musthave', sysdate, 0);
+--부모테이블에 없는 아이디 이므로 제약조건 위배로 입력되지 않는다.
+insert into board (num, title, content, id, postdate, visitcount)
+    values (seq_board_num.nextval, '제목2입니다.', '내용2입니다.', 'tjoeun', sysdate, 0);
+--커밋 
+commit;
